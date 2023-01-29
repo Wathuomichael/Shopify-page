d2d765b797ef6c42bb597d3bcb381c8df5da582b
Wathuo <wathuomichael@gmail.com>
2023-01-29 18:03:58 +0300

    section 4 css and javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".collapse").forEach((button) => {
+  button.addEventListener("click", () => {
+    const content = button.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+});
