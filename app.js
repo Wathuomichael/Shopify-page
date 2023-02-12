672bee5479d585cafe51f2cb555b22815174a22a
Wathuo <wathuomichael@gmail.com>
2023-02-12 15:14:23 +0300

    media specific css

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const closeButtons = document.querySelectorAll(".close");
+for (let i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener("click", function () {
+    this.parentElement.style.display = "none";
+  });
+}
+
 document.querySelectorAll(".collapse").forEach((button) => {
   button.addEventListener("click", () => {
     const content = button.nextElementSibling;
